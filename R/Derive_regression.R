0951850fbd2898dd4c24c05388c9780a66e892a6
qureshlatif <qureshlatif@yahoo.com>
2023-10-03 11:05:59 -0600

    add Derive_regression function

---
+++ R/Derive_regression.R
@@ -0,0 +1,17 @@
+Derive_regression <- function(y, x) {
+  require(MASS)
+  if(length(x) != dim(y)[2]) stop("Length of x must equal ncol(y) for 'Derive_regression'.")
+  nsamp <- dim(y)[1]
+  nx <- length(x)
+  intercept <- slope <- numeric(length = nsamp)
+  for(i in 1:nsamp) {
+    md <- lm(y[i, ] ~ x)
+    mn <- coefficients(md)
+    vc <- vcov(md)
+    cfs <- MASS::mvrnorm(1, mn, vc)
+    intercept[i] <- cfs["(Intercept)"]
+    slope[i] <- cfs["x"]
+  }
+  return(list(intercept = intercept,
+              slope = slope))
+}
