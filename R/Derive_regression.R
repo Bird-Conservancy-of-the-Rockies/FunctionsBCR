cd8c03ff7cfd14b986819cb58a926243209eb3e8
Quresh Latif <qureshlatif@yahoo.com>
2024-03-19 13:00:48 -0600

    Add option to Derive_regression
    Adds option to turn off propagation of uncertainty from the fitted linear model.

--- R/Derive_regression.R
+++ R/Derive_regression.R
@@ -1,4 +1,4 @@
-Derive_regression <- function(y, x) {
+Derive_regression <- function(y, x, propagate_uncertainty = TRUE) {
   if(length(x) != dim(y)[2]) stop("Length of x must equal ncol(y) for 'Derive_regression'.")
   nsamp <- dim(y)[1]
   nx <- length(x)
@@ -6,8 +6,12 @@ Derive_regression <- function(y, x) {
   for(i in 1:nsamp) {
     md <- lm(y[i, ] ~ x)
     mn <- coefficients(md)
-    vc <- vcov(md)
-    cfs <- MASS::mvrnorm(1, mn, vc)
+    if(propagate_uncertainty) {
+      vc <- vcov(md)
+      cfs <- MASS::mvrnorm(1, mn, vc)
+    } else {
+      cfs <- mn
+    }
     intercept[i] <- cfs["(Intercept)"]
     slope[i] <- cfs["x"]
   }
