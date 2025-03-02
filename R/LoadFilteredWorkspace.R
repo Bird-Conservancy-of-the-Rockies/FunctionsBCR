27372172ea5b2b4323337eba78f1fe8e543a65ff
Quresh Latif <qureshlatif@yahoo.com>
2025-03-02 09:46:59 -0700

    add LoadFilteredWorkspace function

---
+++ R/LoadFilteredWorkspace.R
@@ -0,0 +1,17 @@
+loadFilteredWorkspace <- function(workspace, objects.exclude = c(), target.envir) {
+  
+  # Create a temporary environment
+  temp_env <- new.env()
+  
+  # Load the .RData file into the temporary environment
+  load(workspace, envir = temp_env)
+  
+  # Remove unwanted objects from the temporary environment
+  rm(list = objects.exclude, envir = temp_env)
+  
+  # Copy remaining objects to the global environment
+  list2env(as.list(temp_env), envir = envir)
+  
+  # Clean up
+  rm(temp_env)
+}
