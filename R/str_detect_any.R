816e7a7782186589b9a7ebeeb61b942a5e52fab1
Quresh Latif <qureshlatif@yahoo.com>
2025-11-18 13:06:37 -0700

    update str_detect_any
    Add option to remove case sensitivity.

--- R/str_detect_any.R
+++ R/str_detect_any.R
@@ -1,12 +1,12 @@
-str_detect_any <- function(string, pattern.vec) {
+str_detect_any <- function(string, pattern.vec, ignore_case = FALSE) {
   require(stringr)
   require(dplyr)
   
   p <- pattern.vec[1]
-  out <- str_detect(string, p)
+  out <- str_detect(string, ifelse(ignore_case, paste0("(?i)", p), p))
   if(length(pattern.vec) > 1) {
     for(p in pattern.vec[-1]) {
-      out2 <- str_detect(string, p)
+      out2 <- str_detect(string, ifelse(ignore_case, paste0("(?i)", p), p))
       out <- cbind(out, out2) %>%
         apply(1, any)
     }
